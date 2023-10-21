f60b0085e833eb79837840727b69db06c71eb226
vinniapr <vinniapriliani427@gmail.com>
2023-10-21 15:46:33 +0700

    first commit

---
+++ tugas praktikum/modul 1/Tugas no1 modul 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+	char nim[] = "2341066";
+	char nama[] = "Vinni Apriliani";
+	char jurusan[] = "Teknik Informatika";
+	
+	printf("nim = %s\n", nim);
+	printf("nama = %s\n", nama);
+	printf("jurusan = %s\n", jurusan);
+	
+	return 0;
+}
+
