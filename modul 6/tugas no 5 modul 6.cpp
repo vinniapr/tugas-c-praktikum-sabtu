c743b245581dcfc38a1019b8912196b61da09626
aryaptr12 <vinniapriliani427@gmail.com>
2023-11-27 20:15:30 +0700

    first commit

---
+++ modul 6/tugas no 5 modul 6.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main () {
+ 	double saldo = 1000000;
+ 	double bunga = 0.02;
+ 	int bulan = 10;
+ 	int i;
+ 	
+ 	for (i = 1; i<=bulan; i++) {
+ 		saldo +=saldo*bunga;
+	 }
+	 
+	 printf("jumlah uang setelah %d bulan:Rp. %.2f", bulan, saldo);
+	 
+	 return 0;
+ }
